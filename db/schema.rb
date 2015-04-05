68625121e222bd9fe69ffd0977d66e1b205dd81c
Elizabeth Blackburn <e.r.blackburn@gmail.com>
2015-04-05 19:05:04 -0400

    migrated from sqlite3 to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150405212515) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.string   "name"
     t.text     "body"
@@ -21,7 +24,7 @@ ActiveRecord::Schema.define(version: 20150405212515) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -35,4 +38,5 @@ ActiveRecord::Schema.define(version: 20150405212515) do
     t.string "password_digest"
   end
 
+  add_foreign_key "comments", "posts"
 end
